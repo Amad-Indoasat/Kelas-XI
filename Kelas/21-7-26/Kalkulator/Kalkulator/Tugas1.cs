02549bf5d8929fb15fe22b23e01fa9c0f5cf267e
Dayy <Dayy@DESKTOP-QCP8FLQ>
2026-07-21 21:55:01 +0200

    Tugas 7/21/26

---
+++ Kelas/21-7-26/Kalkulator/Kalkulator/Tugas1.cs
@@ -0,0 +1,33 @@
+﻿namespace tugas1
+{
+    public class kalkulator
+    {
+        public double Tambah(double a, double b) => a + b;
+        public double Kurang(double a, double b) => a - b;
+        public double Kali(double a, double b) => a * b;
+        public double Bagi(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Pembagian dengan nol tidak diperbolehkan!");
+            }
+            return a / b;
+        }
+    }
+
+    public class bangunDatar
+    {
+        public double LuasPersegi(double sisi) => sisi * sisi;
+        public double KelilingPersegi(double sisi) => 4 * sisi;
+        public double LuasPersegiPanjang(double p, double l) => p * l;
+        public double KelilingPersegiPanjang(double p, double l) => 2 * (p + l);
+        public double LuasLingkaran(double r) => Math.PI * r * r;
+    }
+
+    public class bangunRuang
+    {
+        public double VolumeKubus(double sisi) => Math.Pow(sisi, 3);
+        public double VolumeBalok(double p, double l, double t) => p * l * t;
+        public double VolumeTabung(double r, double t) => Math.PI * Math.Pow(r, 2) * t;
+    }
+}
